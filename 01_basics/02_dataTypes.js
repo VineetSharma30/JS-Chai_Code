c3630e88169641cd6440ad85708a3c5cd2d2505f
Vineet <imvineet.sh@gmail.com>
2025-08-04 15:42:16 +0530

    learned about datatypes in js

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,22 @@
+"use strict"; //treat all JS code as a newer version
+// Nowadays it's not required the system automatically do it.
+
+// alert("Hello"); // works on browser 
+
+// Important Documentation : tc39.es , mdn
+
+let name = "Vineet" // string
+let age = 19   // number
+let isLoggedIn = true  // boolean
+let status = null // null is standalone value
+
+// undefined => when a variable is not defined before accesing
+
+// symbol => unique
+// object => null is a type of object
+
+console.log(typeof undefined); // undefined 
+console.log(typeof null); // oblect
+
+
+
