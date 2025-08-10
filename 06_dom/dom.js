35f479dbe611e740d44dbf0ed328ea207ee0d479
Vineet <imvineet.sh@gmail.com>
2025-08-10 23:52:03 +0530

    Querry Selectors DOM

---
+++ 06_dom/dom.js
@@ -0,0 +1,19 @@
+// Different types of querry Selectors
+
+// document.getElementById("title")
+// document.getElementsByClassName("heading")
+// document.getElementsByTagName("div")
+
+// Attributes
+
+// document.getElementById("title").getAttribute("class")
+// document.getElementById("title").setAttribute("class","heading new")
+
+const title =  document.getElementById("title")
+title.style.backgroundColor  = "lightgreen"
+title.style.padding = "15px"
+title.style.borderRadius = "15px"
+
+//
+
+title.innerText 
